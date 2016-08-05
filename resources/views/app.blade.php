b8c8e16b18396712dbcecfb83546226cc183246e
eljun <el.junsan@yahoo.com>
2016-08-05 15:31:36 +0800

    Added amplitude

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,6 +24,7 @@
     </script>
 
     <script type="text/javascript" src="{{url(elixir('js/app.min.js'))}}"></script>
+    <script type="text/javascript" src="{{url(elixir('js/amplitude.js'))}}"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -42,4 +43,10 @@
         </div>
     </div>
 </body>
+<script type='text/javascript'>
+  (function() {
+    amplitude.logEvent('slackin invite page');
+  })();
+</script>
+
 </html>
